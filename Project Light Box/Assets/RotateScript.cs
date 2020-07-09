0cd0ec303be5db72934e7a292440ecadeb6d5048
Andreas Larsson <zlaktarn@gmail.com>
2020-07-09 16:55:42 +0200

    Update

---
+++ Project Light Box/Assets/RotateScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+
+    Vector3 rotation;
+    Transform rotate;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = 20* Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.A))
+            {
+            gameObject.transform.Rotate(0, -x, 0);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+            {
+            gameObject.transform.Rotate(0, x, 0);
+
+            }
+    }
+}
---
+++ Project Light Box/Assets/SpriteShadow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteShadow : MonoBehaviour
+{
+    Renderer renderer;
+
+
+    public bool recieveShadows;
+    public bool castShadows;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+
+        renderer.receiveShadows = recieveShadows;
+
+        if(castShadows)
+            renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
